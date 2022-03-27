c4e825ea1085f01df7e68a4c3edc25ba329ad2a6
dragonworx <dragonworxau@yahoo.com.au>
2022-03-27 22:36:19 +1100

    remove rewrite files

--- src/core/layout/horizontal.ts
+++
@@ -1,8 +0,0 @@
-import Rectangle from 'src/lib/rectangle';
-import WrapLayout from './wrap';
-
-export default class HorizontalLayout extends WrapLayout {
-  isValidBounds(_bounds: Rectangle): boolean {
-    return true;
-  }
-}
